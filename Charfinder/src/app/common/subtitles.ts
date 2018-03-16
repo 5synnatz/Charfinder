88eff71325cb2b8666f1b6d5ee35ce13dff29b9d
5synnatz <5synnatz@informatik.uni-hamburg.de>
2018-03-16 20:24:12 +0100

    Subtitles Fixed

--- Charfinder/src/app/common/subtitles.ts
+++ Charfinder/src/app/common/subtitles.ts
@@ -1,10 +1,10 @@
 
 
 export class Subtitles {
-  public readonly Start = 'Create-A-Charakter';
+  public readonly Start = 'Es geht los!';
   public readonly Person = 'Wer ist Dein Charakter?';
   public readonly Skills = 'Welche Fähigkeiten hat Dein Charakter?';
-  public readonly Reactions = 'Welche reagiert Dein Charakter?';
+  public readonly Reactions = 'Wie reagiert Dein Charakter?';
   public readonly Origin = 'Woher kommt Dein Charakter?';
   public readonly Goals = 'Wohin geht Dein Charakter?';
   public readonly Contacts = 'Welche Kontakte hat Dein Charakter?';
